758795c420a0dffedfecb0f851f0904f92f89104
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-19 20:36:57 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,17 +10,17 @@ module.exports = {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly'
   },
-    'parserOptions': {
-        'ecmaVersion': 2018
-    },
-    'rules': {
-        'indent': [
-            'warn',
-             2
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'indent': [
+      'warn',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
         ],
         'quotes': [
             'warn',
