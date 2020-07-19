af77760e41f22f07f752783d1dc2e6fd8507d9ed
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-19 20:29:29 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es6': true,
-        'node': true
-    },
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true 
+  },
     'extends': 'eslint:recommended',
     'globals': {
         'Atomics': 'readonly',
