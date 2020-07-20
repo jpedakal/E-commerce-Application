ef8adb61948acebde2fa76e9b8a871c6b6cfb8d8
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-20 10:40:53 +0530

    Update purchase.js

--- src/api/user/purchase.js
+++ src/api/user/purchase.js
@@ -37,7 +37,7 @@ router.post('/purchase', passport.authenticate('jwt', { session: false }), (req,
     order_data.ordered_on = new Date(),
     mongo.findDocuments('user', FilterCondition)
       .then(data => {
-    //    const new_orders = data[0].past_orders.push(order_data);
+      // const new_orders = data[0].past_orders.push(order_data);
         mongo.updateDocument('user', FilterCondition, { past_orders: data[0].past_orders })
           .then(result => res.status(200).json(result))
           .catch(err => res.json(err));
