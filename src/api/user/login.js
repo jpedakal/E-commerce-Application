fb9a37bd41b140f2fc0d8481d1c3586d1a61f9ad
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-23 17:19:47 +0530

    Update login.js

--- src/api/user/login.js
+++ src/api/user/login.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mongo = require('../../database/mongo_db');
 
-
 router.post('/login', (req, res) => {
   const payload = {
     mobile: req.body.mobile,
