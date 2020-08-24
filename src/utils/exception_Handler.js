5b5b9180e0c57d7cd3be2c7382ec89cc0e693d55
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-24 10:22:31 +0530

    Update exception_Handler.js

--- src/utils/exception_Handler.js
+++ src/utils/exception_Handler.js
@@ -4,5 +4,19 @@ exports.exceptionHandler = function (err) {
         return `Unable to connect to target application,  please try after sometime!`
     } else if (status === 400) {
         return `The target system could not understand the request due to invalid syntax. Please connect with Addison support team`
-    } else if (status === 401)
+    } else if (status === 401){
+        return `Authorization error on target system; missing valid credentials! Please connect with Addison support team`
+    } else if(status===403){
+        return `The access to the requested resource on target system is forbidden! Please connect with Addison support team`
+    } else if(status===413){
+        return `The access to the requested resource on target system is forbidden! Please connect with Addison support team`
+    }else if(status===414){
+        return `The access to the requested resource on target system is forbidden! Please connect with Addison support team`
+    }else if(status===500){
+        return `The access to the requested resource on target system is forbidden! Please connect with Addison support team`
+    }else if(status===503){
+        return `The access to the requested resource on target system is forbidden! Please connect with Addison support team`
+    }else if(status===504){
+        return `The access to the requested resource on target system is forbidden! Please connect with Addison support team`
+    }
 }
