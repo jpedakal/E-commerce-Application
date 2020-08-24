685feb06f1c15a132bdaad62a1abb88424b22032
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-24 10:18:54 +0530

    Update exception_Handler.js

--- src/utils/exception_Handler.js
+++ src/utils/exception_Handler.js
@@ -1,3 +1,8 @@
-exports.exceptionHandler= function(err){
-
+exports.exceptionHandler = function (err) {
+    let status = err.httpStatusCode;
+    if (status === 404) {
+        return `Unable to connect to target application,  please try after sometime!`
+    } else if (status === 400) {
+        return `The target system could not understand the request due to invalid syntax. Please connect with Addison support team`
+    } else if (status === 401)
 }
