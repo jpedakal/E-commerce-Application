567b82975398fb0cf74a55493f170ca92064017d
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-25 15:49:38 +0530

    Update delete_cart.js

--- src/api/user/delete_cart.js
+++ src/api/user/delete_cart.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const mongo = require('../../database/mongo_db');
 const passport = require('passport');
 
-router.delete('/delete_cart', passport.authenticate('jwt', { session: false }), (req, res) => {
-  
-  const payload = {cpf: req.user[0].cpf};
-  mongo.deleteCartItem('user', payload)
-    .then(data=> res.status(200).json(data[0].cart))
-    .catch(err=> res.json(err));
-}); 
+router.delete('/delete_cart/:title', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+    const title = req.params.title;
+    const payload = { cpf: req.user[0].cpf };
+    mongo.deleteCartItem('user', payload, title)
+        .then(data => res.status(200).json({"message":"item removed from cart successfully"}))
+        .catch(err => res.json(err));
+});
 
 module.exports = router;
