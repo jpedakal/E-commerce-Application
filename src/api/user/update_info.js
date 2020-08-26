ee5132337688741d64d860747ca0c0b68af79d31
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-26 18:22:51 +0530

    Update update_info.js

--- src/api/user/update_info.js
+++ src/api/user/update_info.js
@@ -6,7 +6,7 @@ const mongo = require('../../database/mongo_db');
 router.post('/update_user_info', passport.authenticate('jwt', { session: false }), (req, res) => {
     const filterConditon = { cpf: req.user[0].cpf };
     const payload = req.payload;
-    mongo.updateDocument(filterConditon, payload)
+    mongo.updateDocument('user', filterConditon, payload)
         .then(doc => res.json(doc))
         .catch(err => res.json(err));
 });
