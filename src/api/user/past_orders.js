e65776e734ed6415702f74fec80e53a8d8263f3b
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-26 17:06:47 +0530

    Update past_orders.js

--- src/api/user/past_orders.js
+++ src/api/user/past_orders.js
@@ -1,2 +1,7 @@
-const express= require('express');
-const router= express.Router();
+const express = require('express');
+const router = express.Router();
+const mongo = require('../../database/mongo_db');
+const passport = require('passport');
+
+
+router.get('/past_orders', pa)
