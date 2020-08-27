e7f6c69bdb4b279ac682a47d5f5d393f771edfe1
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-27 17:49:41 +0530

    Update fetch_product.js

--- src/api/user/fetch_product.js
+++ src/api/user/fetch_product.js
@@ -6,10 +6,14 @@ router.get('/product_info', (req, res) => {
   const FilterCondition = {
     id: req.query.id
   };
+  if (req.query.id) {
 
-  mongo.findDocuments('product', FilterCondition)
-    .then(data => res.status(200).json(data))
-    .catch(err => res.status(404).json(err));
+  } else {
+    mongo.findDocuments('product', FilterCondition)
+      .then(data => res.status(200).json(data))
+      .catch(err => res.status(404).json(err));
+  }
+
 });
 
 module.exports = router;
