0cdc633f20f12516fd29764738bf8ed933d9a5d4
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-30 14:11:38 +0530

    Update past_orders.js

--- src/api/user/past_orders.js
+++ src/api/user/past_orders.js
@@ -4,13 +4,20 @@ const mongo = require('../../database/mongo_db');
 const passport = require('passport');
 
 router.get('/past_orders', passport.authenticate('jwt', { session: false }), (req, res) => {
-    const filterCondition = { cpf: req.user[0].cpf }
-    mongo.findDocumentsById('user', filterCondition)
-        .then(data => {
-            let output = data.past_orders;
-            res.json(output)
-        })
-        .catch(err => res.json(err));
+    const filterCondition = { cpf: req.user[0].cpf, "past_orders.id": req.query.id }
+    if (req.query.id) {
+        mongo.findDocumentsById('user', filterCondition)
+            .then(doc => {
+                console.log(doc);
+            })
+    } else {
+        mongo.findDocuments('user', filterCondition)
+            .then(data => {
+                let output = data[0].past_orders;
+                res.json(output)
+            })
+            .catch(err => res.json(err));
+    }
 });
 
 module.exports = router;
