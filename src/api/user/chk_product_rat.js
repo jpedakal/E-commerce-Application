28bd2596ef5b4db01fce67e472769b1039b52500
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-08-31 16:30:34 +0530

    Create chk_product_rat.js

---
+++ src/api/user/chk_product_rat.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const mongo = require('../../database/mongo_db');
+const passport = require('passport');
+
+router.get('/product_rating', (req, res) => {
+
+});
