3b0c4ab9a4d77bbc79fcfb7824c85bca219e064f
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-09-02 15:27:22 +0530

    Update admin.js

--- src/config/admin.js
+++ src/config/admin.js
@@ -3,7 +3,6 @@ module.exports = (req, res, next) => {
     next();
   }
   else {
-    res.status(403).json({message: 'You donot have permission to add product. Please contact administrator'});
+    res.status(403).json({message: 'You do not have permission to add product. Please contact administrator'});
   }
 };
-
